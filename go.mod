cc59a72884a1368be74563317dcd5037ced9e8fd
kohama66 <54892265+kohama66@users.noreply.github.com>
2022-10-02 15:54:18 +0900

    add

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.17.20
+	github.com/rs/xid v1.4.0
 	github.com/vektah/gqlparser/v2 v2.5.1
 )
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -5,19 +5,26 @@ package graph
 
 import (
 	"context"
-	"fmt"
 	"gqlgen-example/graph/generated"
 	"gqlgen-example/graph/model"
+
+	"github.com/rs/xid"
 )
 
 // CreateTodo is the resolver for the createTodo field.
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: CreateTodo - createTodo"))
+	todo := &model.Todo{
+		Text: input.Text,
+		ID:   xid.New().String(),
+		User: &model.User{ID: input.UserID, Name: "user " + input.UserID},
+	}
+	r.todos = append(r.todos, todo)
+	return todo, nil
 }
 
 // Todos is the resolver for the todos field.
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented: Todos - todos"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
